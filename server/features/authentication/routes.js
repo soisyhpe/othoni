ac0cc235ebdaa9ce659d2faa2a99b0b41fe2d94c
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-08-24 01:20:17 +0200

    add validations for routes

--- server/features/authentication/routes.js
+++ server/features/authentication/routes.js
@@ -7,9 +7,9 @@ const {
 
 const router = express.Router();
 
-router.post('/register', AuthenticationController.registerUser);
-router.delete('/:username', AuthenticationController.deleteUser);
-router.post('/login', AuthenticationController.loginUser);
+router.post('/register', AuthenticationController.registerUserValidations, AuthenticationController.registerUser);
+router.delete('/:username', AuthenticationController.deleteUserValidations, AuthenticationController.deleteUser);
 router.get('', AuthenticationController.getUsers);
+router.post('/login', AuthenticationController.loginUserValidations, AuthenticationController.loginUser);
 
 module.exports = router;
