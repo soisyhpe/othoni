75557acbd5a00fe488667fbaf0dd4f04501e71f1
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-08-23 14:56:08 +0200

    add .env support

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser');
 const logger = require('./middleware/logger');
 
 const app = express();
-const { port } = require('./config/config');
 const API_PATH = "/api/v1";
 
 logger.info("Starting application...");
+
+// Initialize .env
+function initEnvironment() {
+  require('dotenv').config();
+}
 
 // Initialize configuration
 function initConfig() {
@@ -34,10 +38,10 @@ function initScheduledTask() {
 
 // Initialize routes
 function initRoutes() {
-  // authorizations routes
-  const authorizationsRoutes = require('./features/authorizations/routes');
-  app.use(API_PATH + "/authorizations", authorizationsRoutes);
-  logger.info(`Routes set up for ${API_PATH}/authorizations`);
+  // authentication routes
+  const authenticationRoutes = require('./features/authentication/routes');
+  app.use(API_PATH + "/authentication", authenticationRoutes);
+  logger.info(`Routes set up for ${API_PATH}/authentication`);
 
   // servers routes
   const serversRoutes = require('./features/servers/routes');
@@ -54,14 +58,15 @@ function initErrorHandler() {
 
 // Start the server
 function startServer() {
-  app.listen(port, () => {
-    logger.info(`Server is running on port ${port}`);
+  app.listen(process.env.PORT, () => {
+    logger.info(`Server is running on port ${process.env.PORT}`);
   });
 }
 
 // Initialize the application
 function initializeApp() {
-  const config = initConfig();
+  initEnvironment();
+  initConfig();
   initAuthorization();
   initScheduledTask();
   initRoutes();
