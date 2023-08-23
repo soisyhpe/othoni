34b370ac5764492e4fe1014801e9b32e899ca213
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-08-24 00:39:26 +0200

    add express-validator support

--- server/features/servers/controller.js
+++ server/features/servers/controller.js
@@ -1,4 +1,5 @@
 const cache = require('memory-cache');
+const { check, param, validationResult } = require('express-validator');
 const { defaultOffset, defaultLimit, cacheExpirationInMilliseconds } = require('../../config/config');
 const logger = require('../../middleware/logger');
 const { HTTP_STATUS, SERVER_WITH_PORT_REGEX } = require('../../utils/constantsAndFunctions');
@@ -6,23 +7,30 @@ const ServerServices = require('./services');
 
 const ServerController = {
 
+  // Validation des données pour l'ajout d'un serveur
+  addServerValidations: [
+    check('host').notEmpty().withMessage('Host is required'),
+    check('port').notEmpty().withMessage('Port is required')
+  ],
+
+  // Ajout d'un serveur
   async addServer(req, res) {
     logger.info("Received request for server addition.");
-    const { host, port } = req.body;
 
-    if (!host || !port) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({ message: 'Host and port are required' });
-    }
+    validationCheck(req, res);
+
+    const { host, port } = req.body;
 
     try {
       await ServerServices.addServer({ host: host, port: port });
 
       const cachedKey = `servers_${defaultOffset}_${defaultLimit}`;
-      const cachedServers = cache.get(cachedKey); // Limit here
+      const cachedServers = cache.get(cachedKey);
 
       if (cachedServers) {
         cachedServers.push({ host: host, port: port });
         cache.put(cachedKey, cachedServers, cacheExpirationInMilliseconds);
+        logger.debug(`Added server to cache: ${host}:${port}`);
       }
 
       res.status(HTTP_STATUS.CREATED).json({ message: 'Server added', host: host });
@@ -32,20 +40,32 @@ const ServerController = {
     }
   },
 
+  // ...
+
+  // Suppression d'un serveur
+  deleteServerValidations: [
+    param('server_address_with_port').matches(SERVER_WITH_PORT_REGEX).withMessage((value, { req }) => `'${value}' is not a valid minecraft server address!`)
+  ],
+
   async deleteServer(req, res) {
+    logger.info("Received request for delete server.");
+
+    validationCheck(req, res);
+
+    const server = req.params.server_address_with_port;
+
     try {
-      logger.info("Received request for delete server.");
-      const server = req.params.server_address_with_port;
       const [host, port] = server.split(':');
       const result = await ServerServices.deleteServer({ host: host, port: port });
-      
+
       if (result > 0) {
         const cachedKey = `servers_${defaultOffset}_${defaultLimit}`;
-        const cachedServers = cache.get(cachedKey); // Limit here
+        const cachedServers = cache.get(cachedKey);
 
         if (cachedServers) {
           const updatedCachedServers = cachedServers.filter(s => s.host !== host || s.port !== port);
           cache.put(cachedKey, updatedCachedServers, cacheExpirationInMilliseconds);
+          logger.debug(`Removed server from cache: ${host}:${port}`);
         }
 
         res.status(HTTP_STATUS.ACCEPTED).json({ message: 'Server deleted', deletedCount: result });
@@ -58,24 +78,30 @@ const ServerController = {
     }
   },
 
-  async getServers(req, res) {
-    try {
-      logger.info("Received request for server list.");
-      const offset = req.params.offset || defaultOffset;
-      const limit = req.params.limit || defaultLimit;
+  // ...
 
+  // Récupération des serveurs
+  async getServers(req, res) {
+    logger.info("Received request for server list.");
+
+    validationCheck(req, res);
+
+    const offset = req.params.offset || defaultOffset;
+    const limit = req.params.limit || defaultLimit;
+
+    try {
       const cacheKey = `servers_${offset}_${limit}`;
       const cachedResult = cache.get(cacheKey);
 
       if (cachedResult) {
-        logger.info('Retrieved server list from cache.');
+        logger.debug('Retrieved server list from cache.');
         res.status(HTTP_STATUS.OK).json(cachedResult);
       }
 
       const result = await ServerServices.getServers(offset, limit);
       cache.put(cacheKey, result, cacheExpirationInMilliseconds);
       res.status(HTTP_STATUS.OK).json(result);
-      
+
       logger.info("Server list sent.");
     } catch (error) {
       logger.error('Failed to get server list:', error);
@@ -83,15 +109,19 @@ const ServerController = {
     }
   },
 
+  // ...
+
+  // Récupération d'un serveur
+  getServerValidations: [
+    param('server_address_with_port').matches(SERVER_WITH_PORT_REGEX).withMessage((value, { req }) => `'${value}' is not a valid minecraft server address!`)
+  ],
+
   async getServer(req, res) {
+    logger.info(`Received request for server data`);
+
+    validationCheck(req, res);
+
     const serverAddressWithPort = req.params.server_address_with_port;
-    logger.info(`Received request for server data: ${serverAddressWithPort}`);
-
-    if (!serverAddressWithPort.match(SERVER_WITH_PORT_REGEX)) {
-      logger.warn(`Invalid server address: ${serverAddressWithPort}`);
-      res.status(HTTP_STATUS.BAD_REQUEST).json({ 'Error': `'${serverAddressWithPort}' is not a correct minecraft server address!` });
-    }
-
     const [serverAddress, serverPort] = serverAddressWithPort.split(':');
     const server = { host: serverAddress, port: Number.parseInt(serverPort) || 25565 };
 
@@ -99,19 +129,19 @@ const ServerController = {
     const cachedServerData = cache.get(cachedKey);
 
     if (cachedServerData) {
-      logger.info(`Data retrieved from cache for server: ${serverAddressWithPort}`);
+      logger.debug(`Data retrieved from cache for server: ${serverAddressWithPort}`);
       return res.status(HTTP_STATUS.OK).json(cachedServerData);
     }
 
     try {
       const serverData = await ServerServices.getServer(server);
-  
+
       if (!serverData) {
         logger.warn(`Server not registered: ${serverAddressWithPort}`);
         res.status(HTTP_STATUS.NOT_FOUND).json({ 'Error': `Failed to find recorded data for '${serverAddressWithPort}' in our database!` });
       } else {
         cache.put(cachedKey, serverData, cacheExpirationInMilliseconds);
-        logger.info(`Data retrieved for server: ${serverAddressWithPort}`);
+        logger.debug(`Data retrieved for server: ${serverAddressWithPort}`);
         res.status(HTTP_STATUS.OK).json(serverData);
       }
     } catch (error) {
@@ -119,6 +149,15 @@ const ServerController = {
       res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ 'Error': 'Failed to retrieve server data' });
     }
   }
-}
+};
+
+// Fonction pour vérifier les erreurs de validation et renvoyer une réponse en cas d'erreur
+function validationCheck(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    logger.debug('Validation errors:', errors.array());
+    return res.status(HTTP_STATUS.BAD_REQUEST).json({ errors: errors.array() });
+  }
+} 
 
 module.exports = ServerController;
--- server/features/servers/routes.js
+++ server/features/servers/routes.js
@@ -7,9 +7,9 @@ const {
 
 const router = express.Router();
 
-router.post('', verifyToken, ServerController.addServer);
-router.delete('/:server_address_with_port', verifyToken, ServerController.deleteServer);
-router.get('', verifyToken, ServerController.getServers);
-router.get('/:server_address_with_port', verifyToken, ServerController.getServer);
+router.post('', [verifyToken, ServerController.addServerValidations], ServerController.addServer);
+router.delete('/:server_address_with_port', [verifyToken, ServerController.deleteServerValidations], ServerController.deleteServer);
+router.get('', [verifyToken], ServerController.getServers);
+router.get('/:server_address_with_port', [verifyToken, ServerController.getServerValidations], ServerController.getServer);
 
 module.exports = router;
