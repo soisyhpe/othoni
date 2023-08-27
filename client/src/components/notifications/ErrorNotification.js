cb0049b3ea1cfbf98efe77a767e1b103ef88feff
28720888 <eros.chan@etu.sorbonne-universite.fr>
2023-08-27 17:17:38 +0200

    notifications now expires when not hovered

--- client/src/components/notifications/ErrorNotification.js
+++ client/src/components/notifications/ErrorNotification.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNotification } from '../context/NotificationContext';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,9 +6,10 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const ErrorNotification = () => {
   const { errorMessage, hideNotification } = useNotification();
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
-    if (errorMessage) {
+    if (errorMessage && !isHovered) {
       const timeout = setTimeout(() => {
         hideNotification();
       }, 5000);
@@ -17,17 +18,21 @@ const ErrorNotification = () => {
         clearTimeout(timeout);
       };
     }
-  }, [errorMessage, hideNotification]);
+  }, [errorMessage, hideNotification, isHovered]);
 
   if (!errorMessage) {
     return null;
   }
 
   return (
-    <div className="bg-red-500 bg-opacity-30 p-4 rounded-lg shadow-md absolute backdrop-blur-sm top-8 right-8">
+    <div
+      className="bg-red-500 bg-opacity-50 p-4 rounded-lg border border-red-700 shadow-md absolute backdrop-blur-sm top-8 right-8"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="flex items-center justify-between">
         <p className="text-red-700">{errorMessage}</p>
-        <button className="ml-2 text-red-700" onClick={hideNotification}>
+        <button className="mt-0.5 ml-2 px-1.5 text-red-700" onClick={hideNotification}>
           <FontAwesomeIcon icon={icon({ name: 'xmark' })} />
         </button>
       </div>
--- client/src/components/notifications/SuccessNotification.js
+++ client/src/components/notifications/SuccessNotification.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNotification } from '../context/NotificationContext';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,9 +6,10 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const SuccessNotification = () => {
   const { successMessage, hideNotification } = useNotification();
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
-    if (successMessage) {
+    if (successMessage && !isHovered) {
       const timeout = setTimeout(() => {
         hideNotification();
       }, 5000);
@@ -17,17 +18,21 @@ const SuccessNotification = () => {
         clearTimeout(timeout);
       };
     }
-  }, [successMessage, hideNotification]);
+  }, [successMessage, hideNotification, isHovered]);
 
   if (!successMessage) {
     return null;
   }
 
   return (
-    <div className="bg-green-500 bg-opacity-30 p-4 rounded-lg shadow-md absolute backdrop-blur-sm top-8 right-8">
+    <div
+      className="bg-green-500 bg-opacity-50 p-4 rounded-lg border border-green-700 shadow-md absolute backdrop-blur-sm top-8 right-8"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="flex items-center justify-between">
         <p className="text-green-700">{successMessage}</p>
-        <button className="ml-2 text-green-700" onClick={hideNotification}>
+        <button className="mt-0.5 ml-2 px-1.5 text-green-700" onClick={hideNotification}>
           <FontAwesomeIcon icon={icon({ name: 'xmark' })} />
         </button>
       </div>
